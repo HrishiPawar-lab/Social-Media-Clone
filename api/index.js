f0fa0656eca55105758afdf578bcd9fb8153f94c
Hrishikesh Pawar <122716733+HrishiPawar-lab@users.noreply.github.com>
2024-03-12 23:24:15 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -20,7 +20,14 @@ mongoose.connect(
 
 
 //middleware
-app.use(cors());
+//middleware
+app.use(cors(
+  {
+    origin:['*'],
+    methods:['POST','GET']
+    credentials:true
+  }
+));
 app.use(express.json({ limit: "50mb" }));
 app.use(helmet());
 app.use(morgan("common"));
